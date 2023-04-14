123a9e834154bd5ff4ff438ebe073a65ccf7abde
Gustavo Manzano <74085699+gumanzanoo@users.noreply.github.com>
2023-04-14 10:54:36 -0300

    fcommit

---
+++ database/migrations/2023_04_14_134024_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('data_inicial');
+            $table->dateTime('data_final');
+            $table->double('valor_compra');
+            $table->double('valor_venda');
+            $table->string('descricao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
