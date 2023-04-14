9d527c7a719faf9fb76f3a39d0ba972c27548c89
Gustavo Manzano <74085699+gumanzanoo@users.noreply.github.com>
2023-04-14 16:48:46 -0300

    repository/interface binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\TransactionRepository;
+use App\RepositoriesInterfaces\TransactionInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(TransactionInterface::class, TransactionRepository::class);
     }
 
     /**
